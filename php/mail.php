7e8e7f33e423827abd9e2b1fad6adfd755e9853d
Ashon Shakya <ashon.shakya@javra.com>
2022-01-24 18:26:40 +0545

    update text. update contact form

--- php/mail.php
+++ php/mail.php
@@ -6,15 +6,18 @@
 
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
                 $name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $subject = trim($_POST["subject"]);
+        $contact = trim($_POST["contact"]);
+        $availability = trim($_POST["availability"]);
         $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($name) OR empty($contact) OR empty($availability) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Please complete the form and try again.";
@@ -25,15 +28,46 @@
         // FIXME: Update this to your desired email address.
         $recipient = "info@kukhurikan.com";
 
+        // Full local path to file attachment
+        $file = $_FILES["fileToUpload"]["tmp_name"];
+        $filename = $_FILES["fileToUpload"]["name"];
+
+        // File contents
+        $content = file_get_contents($file);
+        $content = chunk_split(base64_encode($content));
+        $uid = md5(uniqid(time()));
+
+        // Headers for email
+        $header = "From: ".$name." <".$email.">\r\n";
+        $header .= "Reply-To: ".$recipient."\r\n";
+        $header .= "MIME-Version: 1.0\r\n";
+        $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
+
         // Build the email content.
         $email_content = "Name: $name\n";
-        $email_content = "Subject: $subject\n";
         $email_content .= "Email: $email\n\n";
+        $email_content .= "Contact: $contact\n";
+        $email_content .= "Availability: $availability\n";
         $email_content .= "Message:\n$message\n";
+		        
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
-		
+        // Messages and attachment
+        $nmessage = "--".$uid."\r\n";
+        $nmessage .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+        $nmessage .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+        $nmessage .= "Name: $name\n";
+        $nmessage .= "Email: $email\n\n";
+        $nmessage .= "Contact: $contact\n";
+        $nmessage .= "Availability: $availability\n";
+        $nmessage .= "Message:\n$message\n";
+        $nmessage .= "\r\n\r\n";
+        $nmessage .= "--".$uid."\r\n";
+        $nmessage .= "Content-Type: application/octet-stream; name=\"".$filename."\"\r\n";
+        $nmessage .= "Content-Transfer-Encoding: base64\r\n";
+        $nmessage .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
+        $nmessage .= $content."\r\n\r\n";
+        $nmessage .= "--".$uid."--";
+
         // Send the email.
         if (mail($recipient,$subject,$email_content,$email_headers)) {
             // Set a 200 (okay) response code.
