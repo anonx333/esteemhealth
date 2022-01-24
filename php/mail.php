69243006b2b48f65c9f8e218d59d63a97190b289
Ashon Shakya <ashon.shakya@javra.com>
2022-01-24 18:47:22 +0545

    update text. update contact form

--- php/mail.php
+++ php/mail.php
@@ -28,57 +28,67 @@
         // FIXME: Update this to your desired email address.
         $recipient = "info@kukhurikan.com";
 
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
         // Full local path to file attachment
         $file = $_FILES["fileToUpload"]["tmp_name"];
         $filename = $_FILES["fileToUpload"]["name"];
 
-        // File contents
-        $content = file_get_contents($file);
-        $content = chunk_split(base64_encode($content));
-        $uid = md5(uniqid(time()));
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
 
-        // Headers for email
-        $header = "From: ".$name." <".$email.">\r\n";
-        $header .= "Reply-To: ".$recipient."\r\n";
-        $header .= "MIME-Version: 1.0\r\n";
-        $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
+            // File contents
+            $content = file_get_contents($target_file);
+            $content = chunk_split(base64_encode($content));
+            $uid = md5(uniqid(time()));
 
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Contact: $contact\n";
-        $email_content .= "Availability: $availability\n";
-        $email_content .= "Message:\n$message\n";
-		        
+            // Headers for email
+            $header = "From: ".$name." <".$email.">\r\n";
+            $header .= "Reply-To: ".$recipient."\r\n";
+            $header .= "MIME-Version: 1.0\r\n";
+            $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
 
-        // Messages and attachment
-        $nmessage = "--".$uid."\r\n";
-        $nmessage .= "Content-type:text/plain; charset=iso-8859-1\r\n";
-        $nmessage .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-        $nmessage .= "Name: $name\n";
-        $nmessage .= "Email: $email\n\n";
-        $nmessage .= "Contact: $contact\n";
-        $nmessage .= "Availability: $availability\n";
-        $nmessage .= "Message:\n$message\n";
-        $nmessage .= "\r\n\r\n";
-        $nmessage .= "--".$uid."\r\n";
-        $nmessage .= "Content-Type: application/octet-stream; name=\"".$filename."\"\r\n";
-        $nmessage .= "Content-Transfer-Encoding: base64\r\n";
-        $nmessage .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
-        $nmessage .= $content."\r\n\r\n";
-        $nmessage .= "--".$uid."--";
+            // Build the email content.
+            $email_content = "Name: $name\n";
+            $email_content .= "Email: $email\n\n";
+            $email_content .= "Contact: $contact\n";
+            $email_content .= "Availability: $availability\n";
+            $email_content .= "Message:\n$message\n";
+                    
 
-        $subject = "Email from - ".$name;
+            // Messages and attachment
+            $nmessage = "--".$uid."\r\n";
+            $nmessage .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+            $nmessage .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+            $nmessage .= "Name: $name\n";
+            $nmessage .= "Email: $email\n\n";
+            $nmessage .= "Contact: $contact\n";
+            $nmessage .= "Availability: $availability\n";
+            $nmessage .= "Message:\n$message\n";
+            $nmessage .= "\r\n\r\n";
+            $nmessage .= "--".$uid."\r\n";
+            $nmessage .= "Content-Type: application/octet-stream; name=\"".$filename."\"\r\n";
+            $nmessage .= "Content-Transfer-Encoding: base64\r\n";
+            $nmessage .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
+            $nmessage .= $content."\r\n\r\n";
+            $nmessage .= "--".$uid."--";
 
-        // Send the email.
-        if (mail($recipient,$subject,$nmessage,$header)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            $subject = "Email from - ".$name;
+
+            // Send the email.
+            if (mail($recipient,$subject,$nmessage,$header)) {
+                // Set a 200 (okay) response code.
+                http_response_code(200);
+                echo "Thank You! Your message has been sent.";
+            } else {
+                // Set a 500 (internal server error) response code.
+                http_response_code(500);
+                echo "Oops! Something went wrong and we couldn't send your message.";
+            }
         } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            http_response_code(403);
+            echo "Sorry, there was an error uploading your file.";
         }
 
     } else {
